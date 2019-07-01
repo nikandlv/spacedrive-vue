9ce89e3047229e43f1364c0f1edcbf2f9b2636c4
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-01 23:30:46 +0430

    Add more touch to copyright

--- layouts/CopyrightSection.vue
+++ layouts/CopyrightSection.vue
@@ -3,8 +3,8 @@
         <span>
             © 2019 spacedrive.inc
         </span>
-        <a href="https://www.behance.net/gallery/81426537/Spacedrive-Web-UI-KIT?tracking_source=search%7Cfree%20website">Designed By Firos nv with love</a>
-        <a href="https://nikandlv.ir">Coded By Nikandlv with love</a>
+        <legend>Designed By <a href="https://www.behance.net/gallery/81426537/Spacedrive-Web-UI-KIT?tracking_source=search%7Cfree%20website">Firos nv</a> with love</legend>
+        <legend>Coded By <a href="https://nikandlv.ir">Nikandlv</a> with love</legend>
     </div>
 </template>
 <style lang="stylus">
@@ -14,8 +14,10 @@
     border-top 1px solid rgba(white,0.2)
     padding 10px 0
     color white
-    a
+    legend
         margin 0 10px
+        a
+            color coral
     span 
         flex-grow 1
 </style>
