25d080567807164ee221e4427c2b8178a287814c
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-01 22:56:44 +0430

    Added pricing card

---
+++ components/pricingcard/PricingCard.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="pricing-card">
+        <h1><slot name="title">Basic</slot></h1>
+        <span class="space"><slot name="space">500</slot><span class="sub"><slot name="spaceunit">GB</slot></span></span>
+        <p><slot name="domains"></slot> Free Domain</p>
+        <p><slot name="ssd"></slot> GB SSD Disk</p>
+        <p><slot name="extra1">Special Offers</slot></p>
+        <p><slot name="extra2">Unlimited Support</slot></p>
+        <p class="price"><slot name="price">$ 0.80/ Month</slot></p>
+        <span class="buy-now">
+            Buy Now
+        </span>
+    </div> 
+</template>
+<style lang="stylus">
+.pricing-card
+    position relative
+    z-index 10
+    padding 3rem
+    background white
+    border-radius 16px
+    background-image: linear-gradient( #f170ce, #d55e54)
+    .buy-now
+        position absolute
+        bottom 7%
+        right 0px
+        font-size 1rem
+        font-weight bold
+        transform rotate(-45deg)
+        color white
+    &:before
+        content ''
+        background white
+        width 100%
+        height 100%
+        position absolute
+        top 0
+        left 0
+        border-radius 16px
+        border-bottom-right-radius 60%
+    h1
+        font-size 1rem
+        text-transform uppercase
+        color #2708a2
+        padding-bottom 30px
+    p
+        color lighten(black,40)
+        font-size 0.9rem
+    .price
+        color black
+    .space
+        position relative
+        font-size 2rem
+        color black
+        span 
+            position absolute 
+            right -30px
+            top -10px
+            font-size 0.9rem
+</style>
--- layouts/PricingSection.vue
+++ layouts/PricingSection.vue
@@ -1,5 +1,95 @@
 <template>
-    <div>
-        
+    <div class="pricing-section">
+        <h1>Choose your plan</h1>
+        <p>We have many plans that may suite your needs for any web hosting and private servers</p>
+        <div class="cards">
+            <grid>
+            <row center="xs">
+                <column :xs="12" :md="3">
+                    <pricing-card>
+                        <template v-slot:title>Professional</template>
+                        <template v-slot:space>500</template>
+                        <template v-slot:spaceunit>GB</template>
+                        <template v-slot:domains>2</template>
+                        <template v-slot:ssd>300</template>
+                        <template v-slot:price>$ 49.9/ Month</template>
+                    </pricing-card>
+                </column>
+                <column :xs="12" :md="3">
+                    <pricing-card>
+                        <template v-slot:title>Advanced</template>
+                        <template v-slot:space>300</template>
+                        <template v-slot:spaceunit>GB</template>
+                        <template v-slot:domains>1</template>
+                        <template v-slot:ssd>100</template>
+                        <template v-slot:price>$ 29.9/ Month</template>
+                    </pricing-card>
+                </column>
+                <column :xs="12" :md="3">
+                    <pricing-card>
+                        <template v-slot:title>Starter</template>
+                        <template v-slot:space>150</template>
+                        <template v-slot:spaceunit>GB</template>
+                        <template v-slot:domains>No</template>
+                        <template v-slot:ssd>32</template>
+                        <template v-slot:price>$ 12.9/ Month</template>
+                    </pricing-card>
+                </column>
+            </row>
+        </grid>
+        </div>
     </div>
 </template>
+<script>
+import PricingCard from '../components/pricingcard/PricingCard'
+export default {
+    
+    components:{
+        PricingCard
+    }
+}
+</script>
+
+<style lang="stylus">
+.pricing-section
+    background url(../static/img/datacenter-full.jpg) no-repeat center center fixed
+    -webkit-background-size cover
+    -moz-background-size cover
+    -o-background-size cover
+    background-size cover
+    margin-top 150px
+    width 100%
+    box-sizing border-box
+    padding 8rem 8rem
+    color white
+    position relative
+    button 
+        position relative
+        margin-top 30px
+        z-index 1
+    h1
+        font-family: 'Museo', sans-serif
+        position relative
+        z-index 1
+        text-align center
+    p
+        display block
+        margin 0 auto
+        position relative
+        text-align center
+        z-index 1
+        color darken(white,20)
+        font-family: 'Poppins', sans-serif
+        margin-top 2rem
+        max-width 40vw
+    .cards
+        margin-top 50px
+.pricing-section:before
+        content ''
+        width 100%
+        height 100%
+        position absolute 
+        left 0
+        top 0
+        background rgba(#2708a2,0.6)
+</style>
